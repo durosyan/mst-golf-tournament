2da80d1054757ea23c0474bc46061b8531d9ff01
Ryan <52860994+durosyan@users.noreply.github.com>
2023-04-03 11:02:27 +0000

    finished up dep hell

--- golfing-statistics/vite.config.ts
+++ golfing-statistics/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 import analog from '@analogjs/platform';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ mode }: { mode: String }) => ({
   publicDir: 'src/assets',
   build: {
     target: ['es2020'],
